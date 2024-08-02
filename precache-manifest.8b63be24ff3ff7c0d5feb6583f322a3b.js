9fdbab6677a51455038bb8533ab81e60d94d91bc
Priyanshu Chouhan <126661551+Priyanshu-Chouhan@users.noreply.github.com>
2024-08-02 16:34:08 +0530

    Updates

---
+++ precache-manifest.8b63be24ff3ff7c0d5feb6583f322a3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732b8d299da82cf0fefb76f7d95697da",
+    "url": "/E-Commerce-Site/index.html"
+  },
+  {
+    "revision": "ebfd610d73d9ec4c545f",
+    "url": "/E-Commerce-Site/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "068729ebb5e3279d9059",
+    "url": "/E-Commerce-Site/static/css/main.4289a7c4.chunk.css"
+  },
+  {
+    "revision": "ebfd610d73d9ec4c545f",
+    "url": "/E-Commerce-Site/static/js/2.72953f01.chunk.js"
+  },
+  {
+    "revision": "068729ebb5e3279d9059",
+    "url": "/E-Commerce-Site/static/js/main.8803afb0.chunk.js"
+  },
+  {
+    "revision": "8d330f60082c473d0e42",
+    "url": "/E-Commerce-Site/static/js/runtime~main.c1c6698c.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/E-Commerce-Site/static/media/logo.9f7e506d.svg"
+  }
+]);
